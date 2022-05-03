a9a4363548e5778dd0f1e9b003fece3bc2361619
batuhanbasturk <batuhanbasturk@marun.edu.tr>
2022-05-03 21:35:19 +0300

    düzenleme

--- src/SortingAlgorithms.java
+++ src/SortingAlgorithms.java
@@ -179,31 +179,6 @@ public class SortingAlgorithms {
             quickSort(a, p + 1, end);
         }
     }
-    static int kthSmallest(int a[], int left, int right, int k)// quickselect
-    {
-        while (left <= right)
-        {
-
-            // Partition a[left..right] around a pivot
-            // and find the position of the pivot
-            int pivotIndex = partition(a, left, right);
-
-            // If pivot itself is the k-th smallest element
-            if (pivotIndex == k - 1)
-                return a[pivotIndex];
-
-                // If there are more than k-1 elements on
-                // left of pivot, then k-th smallest must be
-                // on left side.
-            else if (pivotIndex > k - 1)
-                right = pivotIndex - 1;
-
-                // Else k-th smallest is on right side.
-            else
-                left = pivotIndex + 1;
-        }
-        return -1;
-    }
 
     public void quickSortTime(int[] a, int start, int end) {
         startTimer();
